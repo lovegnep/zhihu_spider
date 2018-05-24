0ca13985e925d5845bb335c0ce3af89e1926f8ec
lovegnep <1256136108@qq.com>
2018-05-24 13:26:19 +0800

    ddd

--- zhihu/zhihu/tools/async.py
+++ zhihu/zhihu/tools/async.py
@@ -15,11 +15,11 @@ LOGGER = logging.getLogger(__name__)
 @app.task
 def download_pic(image_url, image_path):
     """异步下载图片
-
     Args:
         image_url (string): 图片链接
         image_path (string): 图片路径
     """
+    LOGGER.info('download_pic: %s',image_url)
     if not (image_url and image_path):
         LOGGER.INFO('illegal parameter')
 
