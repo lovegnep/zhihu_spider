ad95a268ea1dc3ed01249748e4f6376e05f310ba
lovegnep <1256136108@qq.com>
2018-05-24 11:17:42 +0800

    ddd

--- zhihu/zhihu/middlewares.py
+++ zhihu/zhihu/middlewares.py
@@ -5,11 +5,11 @@ logger=logging.getLogger()
 
 class monitor(object):
     def process_request(self, request, spider):
-        logger.info('monitor process_request:',request.url)
+        print 'monitor process_request:', request.url
 
     def process_response(request, response, spider):
-        logger.info('monitor process_response:', response.url)
+        print 'monitor process_response:', response.url
         return response
 
     def process_exception(request, exception, spider):
-        logger.info('monitor process_exception:', exception)
+        print 'monitor process_exception:', exception
