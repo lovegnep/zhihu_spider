6159a73e2490bb74cb1ea078b9416f41634db0cf
lovegnep <1256136108@qq.com>
2018-05-24 11:24:07 +0800

    ddd

--- zhihu/zhihu/middlewares.py
+++ zhihu/zhihu/middlewares.py
@@ -1,15 +1,28 @@
 
 import logging
+import codecs
+import datetime
 
 logger=logging.getLogger()
 
 class monitor(object):
     def process_request(self, request, spider):
-        print 'monitor process_request:', request.url
+        logger.info('monitor process_request:%s', request.url)
 
-    def process_response(request, response, spider):
-        print 'monitor process_response:', response.url
+    def process_response(self, request, response, spider):
+        if response.status >= 400:
+            reason = response.status
+            self._faillog(request, u'HTTPERROR', reason, spider)
         return response
 
-    def process_exception(request, exception, spider):
-        print 'monitor process_exception:', exception
+    def process_exception(self, request, exception, spider):
+        self._faillog(request, u'EXCEPTION', exception, spider)
+        return request
+
+    def _faillog(self, request, errorType, reason, spider):
+        with codecs.open('log/faillog.log', 'a', encoding='utf-8') as file:
+            file.write("%(now)s [%(error)s] %(url)s reason: %(reason)s \r\n" %
+                       {'now': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                        'error': errorType,
+                        'url': request.url,
+                        'reason': reason})
